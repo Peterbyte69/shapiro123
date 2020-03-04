8f2f53a907270c10de75eea4523f859af3469b2f
Ben Austin <K035896@eltham-college.org.uk>
2020-03-04 15:42:22 +0000

    kk

--- shapiro123/Program.cs
+++ shapiro123/Program.cs
@@ -1,27 +1,44 @@
 ﻿using System;
+using System.IO;
 
 namespace shapiro123
 {
     class Program
     {
+        static string password;
+        static string username;
         static void Main(string[] args)
         {
 
             int intput1 = 0;
-            Console.WriteLine("Would you like to log in or sign up?\n1 - Log in\n2 - Sign up");
-            Console.Write("Input - ");
-            string input1 = Console.ReadLine();
-            try
+            while(intput1 == 0)
             {
-                intput1 = Convert.ToInt32(input1);
-            }catch
-            {
-                Console.WriteLine("You are rather silly, that's not a number.");
+                Console.WriteLine("Would you like to log in or sign up?\n1 - Log in\n2 - Sign up");
+                Console.Write("\nInput - ");
+                string input1 = Console.ReadLine();
+                try
+                {
+                    intput1 = Convert.ToInt32(input1);
+                }catch
+                {
+                    Console.WriteLine("\nYou are rather silly, that's not a number.");
+                }
+                if(intput1 != 1 & intput1 != 2)
+                {
+                    Console.WriteLine("\nPlease try agian\n");
+                    intput1 = 0;
+                }
+            
             }
-            if(intput1 == 0)
-            {
+        }
 
-            }
+        static void signUp()
+        {
+            StreamReader readerman = new StreamReader("C:/Users/k035896/Documents/computing text files/shapiro123.txt");
+            StreamWriter writerman = new StreamWriter("C:/Users/k035896/Documents/computing text files/shapiro123.txt");
+            Console.WriteLine("Please insert your desired username:");
+            Console.Write("Username = ");
+            Console.ReadLine();
         }
     }
 }
